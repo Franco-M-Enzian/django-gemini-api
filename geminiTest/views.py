0e640350bb428166f3cbe109357b0bdb4e58e262
Franco-M-Enzian <emf0000b@gmail.com>
2024-10-26 01:10:37 +0900

    add geminiTest Views

--- geminiTest/views.py
+++ geminiTest/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from config.settings import API_KEY
+import google.generativeai as genai
 
-# Create your views here.
+
+def index(request):
+    genai.configure(api_key=API_KEY)
+    model = genai.GenerativeModel("gemini-pro")
+    # response = model.generate_content("Djangoの特徴を教えて。")
+    response = model.generate_content("何か文字を一文字だけ出力して。")
+    return HttpResponse(response.text)
